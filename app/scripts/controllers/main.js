9d856adf44cba28dcb10d0c5291fa57057584134
Rafael Bitencourt <rafael.bitencourt@dclick.com.br>
2014-02-12 23:29:13 -0200

    environment start

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularDcjsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
